f16908d3e084190a0c2822d96df7f7aa48937c20
Daniel Garnier-Moiroux <git@garnier.wf>
2018-05-22 17:52:28 +0200

    rename "check" method to "run_check"

--- src/test/test_check.py
+++ src/test/test_check.py
@@ -3,7 +3,7 @@ import unittest
 from datetime import datetime
 from unittest.mock import patch
 
-from weather.check import check
+from weather.check import run_check
 from weather.models import EmptyVersion, Source, Version
 
 
@@ -21,19 +21,19 @@ class TestCheck(unittest.TestCase):
         with patch('requests.get') as mock_request:
             mock_request.return_value = self.response_sunny
 
-            version = check(self.tokyo)[0]
+            version = run_check(self.tokyo)[0]
             self.assertEqual(version.weather, "sunny")
             self.assertEqual(version.date, self.created_time)
 
     def test_contacts_yahoo(self):
         with patch('requests.get') as mock_request:
-            check(self.tokyo)
+            run_check(self.tokyo)
             url = mock_request.call_args[0]
             self.assertIn("https://query.yahooapis.com/v1/public/yql", url)
 
     def test_checks_weather_for_city(self):
         with patch('requests.get') as mock_request:
-            check(Source("paris, france"))
+            run_check(Source("paris, france"))
             yql = 'select item.condition from weather.forecast where woeid in (select woeid from geo.places(1) where text="paris, france")'
             url = mock_request.call_args[0]
             self.assertIn({'q': yql, 'format': 'json'}, url)
@@ -42,21 +42,21 @@ class TestCheck(unittest.TestCase):
         with patch('requests.get') as mock_request:
             mock_request.return_value = self.response_sunny
 
-            versions = check(self.tokyo, Version("cloudy", ""))
+            versions = run_check(self.tokyo, Version("cloudy", ""))
             self.assertGreaterEqual(len(versions), 1)
 
     def test_no_version_on_same_weather(self):
         with patch('requests.get') as mock_request:
             mock_request.return_value = self.response_sunny
 
-            versions = check(self.tokyo, Version("sunny", ""))
+            versions = run_check(self.tokyo, Version("sunny", ""))
             self.assertEqual(len(versions), 0)
 
     def test_new_version_when_previous_empty(self):
         with patch('requests.get') as mock_request:
             mock_request.return_value = self.response_sunny
 
-            versions = check(self.tokyo, EmptyVersion())
+            versions = run_check(self.tokyo, EmptyVersion())
             self.assertEqual(len(versions), 1)
 
 
--- src/weather/check.py
+++ src/weather/check.py
@@ -8,7 +8,7 @@ import requests
 from weather.models import EmptyVersion, Source, TVersion, Version, load_source_version
 
 
-def check(source: Source, previous_version: TVersion = EmptyVersion()) -> List[Version]:
+def run_check(source: Source, previous_version: TVersion = EmptyVersion()) -> List[Version]:
     url = 'https://query.yahooapis.com/v1/public/yql'
     query = 'select item.condition from weather.forecast where woeid in (select woeid from geo.places(1) where text="%s")' % source.city
     response = requests.get(url, {'q': query, 'format': 'json'})
@@ -23,5 +23,5 @@ def check(source: Source, previous_version: TVersion = EmptyVersion()) -> List[V
 
 if __name__ == "__main__":
     source, version = load_source_version(sys.stdin.read())
-    new_versions = check(source, version)
+    new_versions = run_check(source, version)
     print(json.dumps([v.to_dict() for v in new_versions]))
