073a798739108ebd44c42baa3c5a097175f6ac61
Daniel Garnier-Moiroux <git@garnier.wf>
2018-05-23 08:43:45 +0200

    in: writes the result of getting the resource to a file
    - Kinda cheating: we can't fetch a specific version of a
      resource, you'll always get the current weather forecast

--- src/test/test_in.py
+++ src/test/test_in.py
@@ -1,8 +1,10 @@
+import os.path
+import tempfile
 import unittest
 from unittest.mock import patch
 
-from weather.models import Version, Source
 from weather.in_cmd import run_in
+from weather.models import Source, Version
 
 
 class TestIn(unittest.TestCase):
@@ -12,13 +14,29 @@ class TestIn(unittest.TestCase):
     def setUp(self):
         self.created_time = '2018-05-21T16:26:37Z'
         self.source = Source("tokyo, japan")
+        self.input_dir = tempfile.mkdtemp()
 
     def test_fetches_from_yahoo_weather(self):
         with patch('weather.yahoo_weather.fetch') as mock_fetch:
             mock_fetch.return_value = Version("sunny", self.created_time)
-            run_in(self.source)
+            run_in(self.source, self.input_dir)
 
             mock_fetch.assert_called_once_with(self.source)
+
+    def test_creates_txt_file(self):
+        with patch('weather.yahoo_weather.fetch') as mock_fetch:
+            mock_fetch.return_value = Version(
+                "lovely weather", self.created_time)
+            run_in(self.source, self.input_dir)
+
+            weather_file = os.path.join(self.input_dir, "weather.txt")
+            self.assertTrue(os.path.isfile(weather_file))
+
+            file_content = None
+            with open(weather_file, "r") as f:
+                file_content = f.read()
+
+            self.assertEqual(file_content, "lovely weather")
 
 
 if __name__ == '__main__':
--- src/weather/in_cmd.py
+++ src/weather/in_cmd.py
@@ -1,9 +1,15 @@
+import os.path
+
 import weather.yahoo_weather as yahoo_weather
 from weather.models import Source, Version
 
-def run_in(source: Source) -> Version:
-    return yahoo_weather.fetch(source)
+
+def run_in(source: Source, input_dir: str) -> None:
+    version = yahoo_weather.fetch(source)
+    weather_file = os.path.join(input_dir, "weather.txt")
+    with open(weather_file, "w") as f:
+        f.write(version.weather)
+
 
 if __name__ == "__main__":
     pass
-
